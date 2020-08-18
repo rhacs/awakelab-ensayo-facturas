0feb04640c9b9891cb4b66e435bd1926327feafb
Ricardo A. Hermosilla Carrillo <33528976+rhacs@users.noreply.github.com>
2020-08-18 18:55:29 -0400

    added constant for factura's table

---
+++ src/main/java/io/github/rhacs/facturas/Constantes.java
@@ -0,0 +1,20 @@
+package io.github.rhacs.facturas;
+
+public class Constantes {
+
+    // Tablas
+    // -----------------------------------------------------------------------------------------
+
+    /**
+     * Nombre de la tabla que contiene la información de las {@link Factura}s
+     */
+    public static final String TABLA_FACTURAS = "factura";
+
+    // Constructores
+    // -----------------------------------------------------------------------------------------
+
+    private Constantes() {
+        // Constructor privado para esconder el constructor público implícito
+    }
+
+}
